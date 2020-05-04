66b78cc42c0fbe4fb7b7e1584c60e71b13fc2b32
Kilea Castillo <kileaw@gmail.com>
2020-05-03 19:25:24 -0600

    html pages looking sleek

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,9 @@
 var path = require("path");
+var friends = require('../data/friends.js');
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
        res.sendFile(path.join(__dirname, "/../data/friends.js")); 
     });
 }
+
